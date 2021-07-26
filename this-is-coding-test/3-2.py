fabb41ccdd352c5e9316d350637258f53e59cddf
Song Sungyoub <ssyoub0327@daum.net>
2021-07-26 16:09:43 +0900

    move the files

--- this-is-coding-test/3-2.py
+++
@@ -1,8 +0,0 @@
-n, m, k = map(int, input().split())
-data = list(map(int, input().split()))
-
-data.sort(reverse=True)
-
-result = (data[0]*k+data[1])*(m//(k+1)) + data[0]*(m%(k+1))
-
-print(result)
