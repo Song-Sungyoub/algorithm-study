de4cf670818c30e411a10614e366648bab5ca531
Song Sungyoub <ssyoub0327@daum.net>
2021-07-28 12:55:42 +0900

    new day

--- this-is-coding-test/implementation/4-3.py
+++ this-is-coding-test/implementation/4-3.py
@@ -1,1 +1,14 @@
 n = input()
+
+x, y = ord(n[0]) - ord('a') + 1, int(n[1])
+
+moving = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+cnt = 0
+
+for mx, my in moving:
+	mx = x+mx
+	my = y+my
+	if 0<mx<=8 and 0<my<=8:
+		cnt += 1
+		
+print(cnt)
---
+++ this-is-coding-test/implementation/4-4.py
@@ -0,0 +1,3 @@
+n, m = map(int, input().split())
+x,y,d = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
