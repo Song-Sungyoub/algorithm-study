c8b8dcfd514e583a06e5e6075f45e70045cc75ab
SongSungyoub <>
2021-08-08 06:34:41 +0000

    dfs

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,9 +1,41 @@
+from collections import deque
+
 n,m,v = map(int, input().split())
 
-graph = [[] * n]
+graph = [[] for _ in range(n+1)]
 for _ in range(m):
   a,b = map(int, input().split())
   graph[a].append(b)
+  graph[b].append(a)
 
-print(graph)
+for i in range(len(graph)):
+	graph[i].sort()
 
+def dfs(graph, v, visited):
+	visited[v] = True
+	print(v, end=' ')
+	for i in graph[v]:
+		if not visited[i]:
+			dfs(graph, i, visited)
+
+def bfs(graph, start, visited):
+	visited[start] = True
+	queue = deque([start])
+
+	while queue:
+		v = queue.popleft()
+		print(v, end=' ')
+		for i in graph[v]:
+			if not visited[i]:
+				visited[i] = True
+				queue.append(i)
+
+
+visited = [False]*(n+1)
+
+dfs(graph, v, visited)
+print()
+
+visited= [False]*(n+1)
+
+bfs(graph, v, visited)
