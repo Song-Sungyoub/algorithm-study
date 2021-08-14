a1ebb3d82a2c52f1ac374bfa1c1678cff30313c5
SongSungyoub <>
2021-08-14 13:17:45 +0000

    show me

--- baekjoon/2108.py
+++ baekjoon/2108.py
@@ -1,13 +1,19 @@
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+def find_keys(dict, val):
+	a = list(key for key, value in dict.items() if value == val)
+	if len(a) > 1:
+		return a[1]
+	return a[0]
 
-s_arr = list(set(arr))
+n = int(input())
+arr = sorted([int(input()) for _ in range(n)])
+
 d_arr = {}
-for i in s_arr:
+for i in set(arr):
 	d_arr[i]=0
 for i in arr:
 	d_arr[i]+=1
-print(f'{sum(arr)/n:.1f}')
+
+print(f'{sum(arr)/n:.0f}')
 print(arr[n//2])
-print(max(d_arr))
-print(max(arr)-min(arr))
+print(find_keys(d_arr, max(d_arr.values())))
+print(arr[-1]-arr[0])
